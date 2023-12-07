0aa20fef4fd22aa80ee991c74b8ff0d22f154a08
KAMILA <dudekkamila@icloud.com>
2023-12-07 12:34:24 +0100

    style scss css section HomeHeader

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -2,15 +2,18 @@ import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import decoration from '../../assets/images/decoration.png'
 import './HomeHeader.scss'
-
+import HeroImage from '../../assets/images/Hero_Image.png'
 const HomeHeader = () => {
     return (
         <header>
             <Link />
             <ScrollLink />
             <div className="header__section">
-                <div className="topMenu">
-                    <a className="topMenu__item" href="">
+                <div className="hero">
+                <img className="hero__img" src={HeroImage} alt="Hero Image" align="left"/>
+                </div>
+             <div className="topMenu">
+                <a className="topMenu__item" href="">
                         <span className="topMenu__login">Zaloguj</span>
                     </a>
 
@@ -37,14 +40,19 @@ const HomeHeader = () => {
                     <a className="navigation__item" href="">
                         <span className="navigation__highlight"> Kontakt </span>
                     </a>
+
                      </div>
                 <div className="intro">
-                    <h1 className="intro__info">Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h1>
+                     <h1 className="intro__info">Zacznij pomagać! <br />Oddaj niechciane rzeczy w zaufane ręce</h1>
                     <img className="intro__img" src={decoration} alt="Decoration"/>
-                    <button className="intro__btn intro__btn-return" >ODDAJ RZECZY</button>
-                    <button className="intro__btn intro__btn-organize" >ZORGANIZUJ ZBIÓRKĘ</button>
                 </div>
+                <div className="btn">
+                    <button className="btn__btn btn__btn-return" >ODDAJ RZECZY</button>
+                    <button className="btn__btn btn__btn-organize" >ZORGANIZUJ ZBIÓRKĘ</button>
+                </div>
+
             </div>
+
         </header>
     );
 };
