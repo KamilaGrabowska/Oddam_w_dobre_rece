04fb62358242cdf3132253a8f5fa03ec1bf3c800
KAMILA <dudekkamila@icloud.com>
2023-12-11 15:43:42 +0100

    added style to HomeSimpleSteps

--- src/components/home/HomeSimpleSteps.jsx
+++ src/components/home/HomeSimpleSteps.jsx
@@ -4,6 +4,7 @@ import Icon_Bag from '../../assets/images/Icon_Bag.png'
 import Icon_search from '../../assets/images/Icon_search.png'
 import Icon_reload from '../../assets/images/Icon_reload.png'
 import {Link} from "react-router-dom";
+import './HomeSimpleSteps.scss'
 
 const HomeSimpleSteps = () => {
 
@@ -14,30 +15,35 @@ const HomeSimpleSteps = () => {
                 <img className="title__img" src={decoration} alt="dekoracja"/>
             </div>
 
-            <div className="col">
-                <img  className="col__img col__img--1" src={Icon_tshirt} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
+            <div className="columnsSteps">
+
+            <div className="colSteps">
+                <img  className="colSteps__img colSteps__img--1" src={Icon_tshirt} alt="Tshirt"/>
+                <h4 className="colSteps__hdl">Wybierz rzeczy</h4>
+                <p className="colSteps__dsc">ubrania, zabawki, sprzęt i inne</p>
             </div>
 
-            <div className="col">
-                <img  className="col__img col__img--2" src={Icon_Bag} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
+            <div className="colSteps">
+                <img  className="colSteps__img colSteps__img--2" src={Icon_Bag} alt="Tshirt"/>
+                <h4 className="colSteps__hdl">Spakuj je</h4>
+
+                <p className="colSteps__dsc">skorzystaj z worków na śmieci</p>
             </div>
 
 
-            <div className="col">
-                <img  className="col__img col__img--3" src={Icon_search} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
+            <div className="colSteps">
+                <img  className="colSteps__img colSteps__img--3" src={Icon_search} alt="Tshirt"/>
+                <h4 className="colSteps__hdl">Zdecyduj komu < br /> chcesz pomóc</h4>
+                <p className="colSteps__dsc">wybierz zaufane miejsce</p>
             </div>
 
-            <div className="col">
-                <img  className="col__img col__img--4" src={Icon_reload} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
+            <div className="colSteps">
+                <img  className="colSteps__img colSteps__img--4" src={Icon_reload} alt="Tshirt"/>
+                <h4 className="colSteps__hdl">Zamów kuriera</h4>
+                <p className="colSteps__dsc">kurier przyjedzie w dogodnym terminie</p>
             </div>
+            </div>
+
             <div className="btn">
                 <Link to="/Login" className="btn__steps" >ODDAJ RZECZY</Link>
             </div>
