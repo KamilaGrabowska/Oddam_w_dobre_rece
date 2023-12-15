927db66961924e5ef1b0c54f9cae547dd1e8d633
KAMILA <dudekkamila@icloud.com>
2023-12-15 11:24:54 +0100

    added sceletor to register component

--- src/components/rejestracja/Register.jsx
+++ src/components/rejestracja/Register.jsx
@@ -1,8 +1,56 @@
+import {Link as RouterLink} from "react-router-dom";
+import decoration from "../../assets/images/decoration.png";
+
 const Register = () => {
     return (
-        <div>
-            <h1>register</h1>
-        </div>
+
+        <section className="register__section">
+            <div className="register__menu">
+                <RouterLink to="/Login" className="register__menu--item" href="">
+                    <a className="register__menu--login">Zaloguj</a>
+                </RouterLink>
+                <RouterLink to="/Register" className="register__menu--item" href="">
+                    <a className="register__menu--register">Załóż konto</a>
+                </RouterLink>
+            </div>
+
+            <div className="register__nav">
+                <a href="/">Start</a>
+                <a href="/">O co chodzi?</a>
+                <a href="/">O nas</a>
+                <a href="/">Fundacja i organizacje</a>
+                <a href="/">Kontakt</a>
+            </div>
+
+            <div className="box">
+                <div className="register__box">
+                    <h2 className="register__box--titleR">Zaloguj się</h2>
+                    <img  className="register__box--imgR" src={decoration} alt="Decoration"/>
+                </div>
+
+                <form  action="" className="register__form">
+                    <label className="register__form--labelR">Email</label>  <br />
+                    <input type="email" id="email" className="register__form--inR" /> <br />
+                    <label className="register__form--labelR"> Hasło </label> <br />
+                    <input type="text" id="haslo" className="register__form--inR"/><br />
+                    <label className="register__form--labelR">  Powtórz hasło </label> <br />
+                    <input type="text" id="Powtorz haslo" className="register__form--inR"/>
+                </form>
+
+                <div className="register_btn">
+                    <button className="regiser__btn--addAccountR">Załóż konto</button>
+                    <button className="register_btn--LoggInR">Zaloguj się</button>
+
+                </div>
+
+
+            </div>
+
+
+
+        </section>
+
+
     );
 };
 
