08c4763e9e324045e8070b0cd9fa51d7b4dd4ac2
KAMILA <dudekkamila@icloud.com>
2024-01-03 14:31:11 +0100

    modified style in componenet Conatact

--- src/components/home/Contact.jsx
+++ src/components/home/Contact.jsx
@@ -8,70 +8,100 @@ const Contact = () => {
     return (
         <section className="contact__section">
 
-            <img  className="contact__img"
-                  src={background}
-                  alt="Blankes on chair"
-                  style={{
-                      width:'1900px',
-                      height:'1026px',
-                  }}
+            <img className="contact__img"
+                 src={background}
+                 alt="Blankes on chair"
+                 style={{
+                     width: '1900px',
+                     height: '1026px',
+                 }}
             />
 
-             <div className="box__right">
-             <h1 className="box__right--hdl">Skontaktuj się z nami</h1>
-            <img className="box__right--decoration" src={decoration} alt="decoration"/>
-             </div>
+            <div className="box__right">
+                <h1 className="box__right--hdl">Skontaktuj się z nami</h1>
+                <img className="box__right--decoration"
+                     src={decoration}
+                     alt="decoration"
+                />
+            </div>
 
             <form action="" className="contact__form">
-                <label  className="contact__form--label">
-                    Wpisz swoje imię
-                </label>
-                <input
-                    type="text"
-                    id="name"
-                    placeholder="Krzysztof"
-                    className="contact__form--in"/>
+                <div className="contact__form--group">
+                    <label className="contact__form--label" htmlFor="name">
+                        Wpisz swoje imię
+                    </label>
+                    <input
+                        type="text"
+                        id="name"
+                        placeholder="Krzysztof"
+                        className="contact__form--in"
+                        style={{
+                            border: 'none',
+                            borderBottom:'1px solid rgba(112, 112, 112, 1)',
 
-                <label  className="contact__form--label">
-                    Wpisz swój email
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    placeholder="abc@xyz.pl"
-                    className="contact__form--in"/>
 
-                <label  className="contact__form--label">
-                    Wpisz swoją wiadomość
+                        }}
+                    />
+                </div>
+
+                <div className="contact__form--group">
+                    <label className="contact__form--label" htmlFor="email">
+                        Wpisz swój email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="abc@xyz.pl"
+                        className="contact__form--in"
+                        style={{
+                            border: 'none',
+                            borderBottom:'1px solid rgba(112, 112, 112, 1)',
+
+                        }}
+                    />
+                </div>
+
+                <div className="contact__form--group">
+                    <label className="contact__form--label" htmlFor="message">
+                        Wpisz swoją wiadomość
+                    </label>
                     <textarea
+                        id="message"
                         name="text"
-                        cols="40"
                         rows="5"
-                        placeholder="Lorem ipsum dolor sit amet,
-                 consectetur adipiscing elit, sed do  eiusmod tempor incididunt ut labore et
-                 dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
-                </label>
+                        cols="20"
+                        placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                         ut aliquip ex ea commodo consequat."
+                        style={{
+                            border: 'none',
+                            borderBottom:'1px solid rgba(112, 112, 112, 1)'
+                        }}
+                    >
 
-                <button className="contact__form__btn--btn">
-                    Wyślij
-                </button>
+                    </textarea>
+                </div>
+
+                <button
+                    className="contact__btn"
+                    type="submit"
+                >Wyślij</button>
             </form>
 
             <div className="footer">
                 <p className="footer__copy">Copyright by Coders Lab</p>
                 <div className="footer__icons">
                     <a href="#" className="footer__icons--icon">
-                        <img src={Istagram} alt="Instagram" />
+                        <img src={Istagram} alt="Instagram"/>
                     </a>
                     <a href="#" className="footer__icons--icon">
-                        <img src={Facebook} alt="Facebook" />
+                        <img src={Facebook} alt="Facebook"/>
                     </a>
                 </div>
             </div>
 
         </section>
-    );
+);
 };
 
 export default Contact;
